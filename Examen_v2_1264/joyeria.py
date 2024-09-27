26f0d197733c194594af7e9deeaba41c230f13b9
Ailin-Miscles-5J <a.22308051281264@cbtis128.edu.mx>
2024-09-27 09:20:33 -0600

    Add files via upload

---
+++ Examen_v2_1264/joyeria.py
@@ -0,0 +1,52 @@
+empleados_1264 = {
+    "Nombre" : "Ailin",
+    "Edad" : 25,
+    "Direccion" : "calle cualquiera 123",
+    "Id_empleado" : 1234,
+    "Telefono" : 6561087676,
+    "Correo" : "ailinruv@gmail.com",
+    "Sexo" : "Femenino"
+}
+print("  Empleados")
+for a, b in empleados_1264.items():
+    print(a, b)
+
+productos_1264 = {
+    "Nombre" : "Anillo",
+    "Id_Producto" : 2535,
+    "Precio Venta" : "$25",
+    "Stock" : 200,
+    "Especificaciones" : "Color rosa gold",
+    "Medidas" : "2cm",
+    "Precio Compra" : "$10"
+}
+print("\n Productos")
+for c, d in productos_1264.items():
+    print(c, d)
+
+
+distribuidores_1264 = {
+    "Nombre" : "Renata",
+    "Telefono" : 6561958205,
+    "Correo" : "renataron@gmil.com",
+    "Ciudad" : "Juarez",
+    "Id_Distribuidor" : 3444,
+    "Edad" : 40,
+    "Costos" : "$300"
+}
+print("\n Disribudores")
+for e, f in distribuidores_1264.items():
+    print(e, f)
+
+sucursal_1264 = {
+    "Direccion" : "Av. Las torres 345",
+    "Telefono" : 6567105295,
+    "Correo" : "sucursaltorres@gail.com",
+    "Horario" : "10 am - 6 pm",
+    "Id_Sucursal" : 21619,
+    "Contidad de Empleados" : 100,
+    "Ciudad" : "Juarez"
+}
+print("\n Sucursal")
+for g, h in sucursal_1264.items():
+    print(g, h)
